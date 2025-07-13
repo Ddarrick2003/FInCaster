1fdd6945fd0e8a0d1b468953a9d4ed4cb38df19c
dabila <mboya4194@gmail.com>
2025-07-14 00:18:30 +0300

    Update garch_model.py

--- model/garch_model.py
+++ model/garch_model.py
@@ -1,1 +1,10 @@
-# Dummy content for garch_model.py
+from arch import arch_model
+
+def forecast_garch_var(df):
+    returns = df['Returns'] * 100  # scaled returns for GARCH
+    am = arch_model(returns, vol='Garch', p=1, q=1)
+    res = am.fit(disp='off')
+    forecast = res.forecast(horizon=5)
+    vol_forecast = forecast.variance.iloc[-1] ** 0.5
+    var_1d = 1.65 * vol_forecast.iloc[0]  # 95% VaR
+    return vol_forecast, var_1d
