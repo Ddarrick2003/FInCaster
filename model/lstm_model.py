4e77db7644461cb129357c8baab69e88d4459417
dabila <mboya4194@gmail.com>
2025-07-14 00:17:56 +0300

    Update lstm_model.py

--- model/lstm_model.py
+++ model/lstm_model.py
@@ -1,1 +1,18 @@
-# Dummy content for lstm_model.py
+import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense
+
+def create_sequences(data, target_col='Close', window_size=10):
+    X, y = [], []
+    for i in range(window_size, len(data)):
+        X.append(data.iloc[i - window_size:i].values)
+        y.append(data[target_col].iloc[i])
+    return np.array(X), np.array(y)
+
+def build_lstm_model(input_shape):
+    model = Sequential()
+    model.add(LSTM(50, return_sequences=True, input_shape=input_shape))
+    model.add(LSTM(50))
+    model.add(Dense(1))
+    model.compile(optimizer='adam', loss='mse')
+    return model
